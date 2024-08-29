bdfb1dbadf66f5b8b737491cbb4a98a3209930a7
Saideep9835 <tsaideep98@gmail.com>
2024-08-29 14:25:25 -0500

    Initial Commit

---
+++ Table-View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table-View
+//
+//  Created by Saideep Reddy Talusani on 8/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
