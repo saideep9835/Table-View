1e0769a39cf42367e08238574689be13a4f071da
Saideep9835 <tsaideep98@gmail.com>
2024-08-30 09:30:13 -0500

    Created a Table view and implemented table view cells with colors.

--- Table-View/ViewController.swift
+++ Table-View/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
+    
+    let rainbow: [UIColor] = [.red, .yellow, .green, .orange, .blue, .purple, .magenta]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rainbow.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+                cell.backgroundColor = rainbow[indexPath.item]
+        //Default Content Configuration
+                var content = cell.defaultContentConfiguration()
+                content.text = rainbow[indexPath.item].accessibilityName.capitalized
+                
+                cell.contentConfiguration = content
+                return cell
+    }
+    
 }
 
